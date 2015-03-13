8c643b5618801dc980057025e8e613c2942fc2a8
Rory Becker <RoryBecker@gmail.com>
2015-03-13 19:16:59 +0000

    - Added Intro Text to 'Use Equality Operator'.

--- Code/Expressions/UseEqualityOperator.cs
+++ Code/Expressions/UseEqualityOperator.cs
@@ -4,7 +4,19 @@ using System.Collections.Generic;
 
 namespace AllCodeRush.Code.Expressions
 {
-    public class UseEqualityOperator
+	/* •———————————————————————————————————————————————————————•
+		  Feature: Use Equality Operator
+          
+		  Use Case: Lets you convert an Equals method call to an 
+	    expression with the equality operator.
+           
+		  Available: When an expression with the Equals method 
+			call is selected.
+          
+		  See also: Introduce Local, Replace with Constant
+	   •———————————————————————————————————————————————————————• */
+
+	public class UseEqualityOperator
     {
         private void TestEquality()
         {
