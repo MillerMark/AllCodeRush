d4ddae0a7e9c854f51d088810d287827df4e5ad6
MillerMark <markm@devexpress.com>
2015-03-13 17:23:35 -0400

    w

--- Code/Conditionals/ConditionalTemplates.cs
+++ Code/Conditionals/ConditionalTemplates.cs
@@ -2,54 +2,27 @@
 
 namespace AllCodeRush.Code.Conditionals
 {
-  public class MyControl
-  {
-    public bool Visible { get; set; }
-    public bool Checked { get; set; }
-    public bool Enabled { get; set; }
-    public bool Sorted { get; set; }
-    public int Height { get; set; }
-    public int Width { get; set; }
-    public int Count { get; set; }
-    public int Length { get; set; }
-    public int Top { get; set; }
-    public int Left { get; set; }
-    public int Right { get; set; }
-    public int Bottom { get; set; }
-    public MyControl Parent { get; set; }
-    public System.Drawing.Rectangle ClientRectangle { get; set; }
-    public string Name { get; set; }
-    public string Hint { get; set; }
-
-    public void Clear()
-    {
-      
-    }
-    public void Dispose()
-    {
-      
-    }
-    public void Close()
-    {
-      
-    }
-  }
   class ConditionalTemplates
   {
     private bool TemplatesForWorkingWithConditions(bool testValue1, bool testValue2, bool testValue3)
     {
       // Basics...
-      // r == return
       // b == {}
+      // r == return
 
       // Primitives:
       //    t == true
       //    f == false
       //    n == null (or Nothing in VB)
-      //    0 == 0
       //    " == ""
       //    ' == ''
-      
+      //    th == this (or Me in VB)
+      //    me == this (or Me in VB)
+      //    se == String.Empty
+      //    1 == 1
+      //    0 == 0
+      //    -1 == -1
+
       // if, ifb, ifn, ifa
 
       // else blocks: l, lf, lb, blb, 
