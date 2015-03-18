3b058a2ee5fcb1918d7c93814df9efa2f5bebdd8
MillerMark <markm@devexpress.com>
2015-03-18 16:01:49 -0400

    w

--- Code/Declaration + Initialization/Support/Customer.cs
+++ Code/Declaration + Initialization/Support/Customer.cs
@@ -2,13 +2,13 @@ using System;
 
 namespace AllCodeRush.Code.DeclarationInitialization
 {
-  public class Person
+  public class Customer
   {
     private readonly string _forename;
     private readonly string _surname;
     private readonly int _age;
     private DateTime _dateOfBirthApprox;
-    public Person(string forename, string surname, int age)
+    public Customer(string forename, string surname, int age)
     {
       _forename = forename;
       _surname = surname;
