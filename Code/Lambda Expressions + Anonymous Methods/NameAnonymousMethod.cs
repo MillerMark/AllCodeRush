f3f99030542f9af9a1013bc408b0364c451b6e0b
MillerMark <markm@devexpress.com>
2015-04-07 18:33:07 -0400

    w

--- Code/Lambda Expressions + Anonymous Methods/NameAnonymousMethod.cs
+++ Code/Lambda Expressions + Anonymous Methods/NameAnonymousMethod.cs
@@ -1,29 +1,28 @@
 using System;
-using System.Linq;
-using System.Collections.Generic;
 
 namespace AllCodeRush.Code.LambdaExpressionsAnonymousMethods
 {
   /* •———————————————————————————————————————————————————————•
       Feature: Name Anonymous Method.
-        
+          
       Use Case: Converts an anonymous method to an instance 
       method.
-         
+           
       Available: When the caret is on the delegate keyword 
       within an anonymous method declaration. 
       The method should not use local variables.
-    
+      
       See also: Inline Delegate
      •———————————————————————————————————————————————————————• */
-  public class NameAnonymousMethod
-	{
-		private void TestMethod()
-		{
-			Func<int, int, int> adder1 = delegate(int x, int y)
-			{
-				return x + y;
-			};
-		}
-	}
+
+  public class NameAnonymousMethod : BaseNameAnonymousMethod
+  {
+    protected override Func<int, int, int> GetOperation()
+    {
+      return delegate(int x, int y)
+      {
+        return x + y;
+      };
+    }
+  }
 }
