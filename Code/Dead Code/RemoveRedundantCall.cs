4440e02effcacbcc39efd5948e6ed9d51496c3ea
MillerMark <markm@devexpress.com>
2015-04-07 17:40:08 -0400

    w

--- Code/Dead Code/RemoveRedundantCall.cs
+++ Code/Dead Code/RemoveRedundantCall.cs
@@ -5,29 +5,29 @@ namespace AllCodeRush.Code.DeadCode
 	/* •———————————————————————————————————————————————————————•
 			Feature: Remove Redundant Call.
         
-			Use Case 1: - Removes a redundant call to a base 
-										constructor.
-			Use Case 2: - Removes a redundant call to String.Format.
+			Use Case: Removes a call to a method that does nothing.
          
 			Available: When the caret is on a call to an empty base 
 			constructor or on a call to String.Format with a single 
 			argument.
+   
+      See also: Inline Method, Inline Method (and delete)
 		 •———————————————————————————————————————————————————————• */
 
 	public class RemoveRedundantCall : BaseAccount
 	{
 		private decimal limit = 0m;
-		public RemoveRedundantCall()
+    public RemoveRedundantCall()
 			: base()
 		{
 		}
 
-		public void Withdraw(decimal value)
-		{
+    public void Withdraw(decimal value)
+    {
       if (balance - value < limit)
-        throw new Exception(string.Format("You have insufficient credit."));
-      else
-        balance = balance - value;
-		}
+        throw new Exception(string.Format("Insufficient credit."));
+      
+      balance = balance - value;
+    }
 	}
 }
