25030af6f6a4ddc6351d0399d490f7f6097aafbb
MillerMark <markm@devexpress.com>
2015-04-09 17:34:44 -0400

    w

--- Code/Methods/PullMemberUp.cs
+++ Code/Methods/PullMemberUp.cs
@@ -2,6 +2,7 @@ using System;
 
 namespace AllCodeRush.Code.Methods
 {
+
   /* •————————————————————————————————————————————————————————•
       Feature: Pull Member Up
         
@@ -10,23 +11,33 @@ namespace AllCodeRush.Code.Methods
       Available: When the caret is on a class member, provided 
       that the base class is declared in the solution.
      •————————————————————————————————————————————————————————• */
-
-  public class Shape2d
+  public class Shape2D
 	{
-    protected double Height;
-    protected double Width;
+    protected double height;
+    protected double width;
 	}
 
-	public class Projection3d : Shape2d
+	public class Projection3D : Shape2D
 	{
-    private double Depth;
+    private double depth;
+    private string name;
+
     public double CalculateArea()
 		{
-			return Width * Height;
+			return width * height;
 		}
     public double CalculateVolume()
 		{
-			return CalculateArea() * Depth;
+			return CalculateArea() * depth;
 		}
-	}
+  
+    public string Name
+    {
+      get { return name; }
+      set
+      {
+        name = value;
+      }
+    }
+  }
 }
