2f531f04e189621c45d3211d554cd2cc72d57715
MillerMark <markm@devexpress.com>
2015-04-10 16:34:25 -0400

    w

--- Code/Methods/DeclareMethod.cs
+++ Code/Methods/DeclareMethod.cs
@@ -12,23 +12,18 @@ namespace AllCodeRush.Code.Methods
         
 			See also: Declare Method (Abstract)
 		 •———————————————————————————————————————————————————————• */
-	public class Account
+  public class Account: BaseAccount
 	{
-		private int _value;
-		public Account(int Value)
+		public Account(int value)
 		{
-			_value = Value;
-		}
-		public void TransferMoney(Account DestAccount, int Value)
-		{
-			this.Withdraw(Value);
-			DestAccount.Deposit(Value);
-      LogBalance(DateTime.Now, DestAccount);
+      balance = value;
 		}
 
-		public void Withdraw(int value)
+    public void TransferMoney(BaseAccount destAccount, int value)
 		{
-			_value -= value;
+			this.Withdraw(value);
+			destAccount.Deposit(value);
+      LogBalance(DateTime.Now, destAccount);
 		}
 	}
 }
